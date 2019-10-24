16592f04c75fd0299610714204aa814ac7d1118d
Youhei Nozaki <youhey.hey0505@gmail.com>
2019-10-24 16:41:26 +0900

    initialize component atom Ballon Heading Img P123

---
+++ src/components/atoms/Balloon/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from "./styles.css";
+
+const Balloon = ({ children, className, ...props }) => (
+  <span className={[styles.balloon, className].join(" ")} {...props}>
+    {children}
+  </span>
+);
+
+export default Balloon;
